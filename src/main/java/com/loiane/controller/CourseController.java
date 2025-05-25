36de69ef0178ecd55c6ef1fd247b3ac419da4dc8
Marisa Vanessa N. Cruz <ideiaselembrancas@gmail.com>
2025-05-25 00:27:22 -0300

    Docker correção 5

--- src/main/java/com/loiane/controller/CourseController.java
+++ src/main/java/com/loiane/controller/CourseController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,7 +20,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+@CrossOrigin(origins = {"https://crud-angular-exmo.onrender.com", "localhost:8081"})
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
--- src/main/java/com/loiane/controller/HelloController.java
+++ src/main/java/com/loiane/controller/HelloController.java
@@ -2,9 +2,10 @@ package com.loiane.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@CrossOrigin(origins = {"https://crud-angular-exmo.onrender.com", "localhost:8081"})
 @RestController
 @RequestMapping("/api/hello")
 public class HelloController {
